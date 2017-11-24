6578af79f6f1ca246219e38608c8b1631cf24a2b
haridu <harindu96@gmail.com>
2017-11-24 16:24:27 +1300

    update to custom vision

--- controller/CognitiveDialog.js
+++ controller/CognitiveDialog.js
@@ -3,13 +3,13 @@ var request = require('request'); //node module for http post requests
 exports.retreiveMessage = function (session){
 
     request.post({
-        url: 'https://customvision.ai/https://southcentralus.api.cognitive.microsoft.com/customvision/v1.0/Prediction/d09183ae-45f8-40c6-88f1-dd8c496f3a08/url?iterationId=c21714ce-026e-4ea5-bd7c-df2945c928e6',
+        url: 'https://southcentralus.api.cognitive.microsoft.com/customvision/v1.0/Prediction/d09183ae-45f8-40c6-88f1-dd8c496f3a08/url?iterationId=c21714ce-026e-4ea5-bd7c-df2945c928e6',
         json: true,
         headers: {
             'Content-Type': 'application/json',
             'Prediction-Key': '18b3ab4dcb65431989aef002aec9799e'
         },
-        body: { 'Url': session.message.text }
+        body: {"Url": "<image url>"}
     }, function(error, response, body){
         console.log(validResponse(body));
         session.send(validResponse(body));
